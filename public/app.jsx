78564953c017ceca2b40cfed59a8c48c9411e6e0
Justin Doty <justindoty12@gmail.com>
2016-10-14 17:26:32 -0500

    intial commit

---
+++ public/app.jsx
@@ -0,0 +1,46 @@
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React',
+      message: 'This is from the component'
+    };
+  },
+  getInitialState: function() {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    this.setState({
+      name: name
+    });
+    
+  },
+  render: function() {
+    var name= this.state.name;
+    var message= this.props.message;
+    return(
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+
+      <form onSubmit={this.onButtonClick}>
+        <input type='text' ref='name'/>
+        <button>Set Name</button>
+      </form>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Justin';
+var newMessage = 'I do what I want!!!!!'
+
+ReactDOM.render(
+  <Greeter name={firstName} message={newMessage}/>,
+  document.getElementById('app')
+);
